4caffb7dde06a5943c91d907a3846f625903027d
brad <brad@brad-MS-7C02>
2026-07-15 14:46:33 +1200

    https://github.com/Kademi/kademi-dev/issues/23834

--- src/main/java/org/hashsplit4j/store/SimpleFileDb.java
+++ src/main/java/org/hashsplit4j/store/SimpleFileDb.java
@@ -12,6 +12,8 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +32,7 @@ public class SimpleFileDb {
     private final File valuesFile;
 
     private final Map<String, DbItem> mapOfItems = new HashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private boolean enabled = false;
 
     /**
@@ -73,18 +76,31 @@ public class SimpleFileDb {
     }
 
     public void replaceData(File newKeysFile, File newValsFile) {
-        enabled = false;
-        mapOfItems.clear();
+        lock.writeLock().lock();
         try {
+            enabled = false;
+            mapOfItems.clear();
             replaceFileContent(newKeysFile, keysFile);
             replaceFileContent(newValsFile, valuesFile);
-            init();
+            initLocked();
         } catch (IOException ex) {
             throw new RuntimeException("Couldnt initialise SimpleDb", ex);
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 
-    public synchronized void init() throws FileNotFoundException, IOException {
+    public void init() throws FileNotFoundException, IOException {
+        lock.writeLock().lock();
+        try {
+            initLocked();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    // must only be called while holding the write lock
+    private void initLocked() throws FileNotFoundException, IOException {
         if (keysFile.exists()) {
             log.warn("init: using keysfile {}", keysFile.getAbsolutePath() );
             try (FileInputStream fin = new FileInputStream(keysFile)) {
@@ -103,61 +119,95 @@ public class SimpleFileDb {
     }
 
     public boolean isEnabled() {
-        return enabled;
+        lock.readLock().lock();
+        try {
+            return enabled;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public int size() {
-        return mapOfItems.size();
+        lock.readLock().lock();
+        try {
+            return mapOfItems.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean contains(String hash) {
-        return mapOfItems.containsKey(hash);
+        lock.readLock().lock();
+        try {
+            return mapOfItems.containsKey(hash);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    public synchronized DbItem put(String key, byte[] val) throws FileNotFoundException, IOException {
-        if (mapOfItems.containsKey(key)) {
-            throw new RuntimeException("Key " + key + " is already present");
-        }
-
-        long startPos;
-        long finishPos;
-        try (FileOutputStream fout = new FileOutputStream(valuesFile, true)) { //open in append mode
-            FileChannel chan = fout.getChannel();
-            startPos = chan.position();
-            ByteBuffer bb = ByteBuffer.wrap(val);
-            chan.write(bb);
-            finishPos = chan.position();
-        }
-        long length = finishPos - startPos;
-        if (length != val.length) {
-            throw new RuntimeException("Inserting blob into simplefiledb failed, lengths differ. Should be " + val.length + " but is " + length + ", for key=" + key);
-        }
-        DbItem dbItem = new DbItem(startPos, finishPos);
-
-        log.info("put: start={} finish={} key={}", startPos, finishPos, key);
-        try (FileOutputStream fout = new FileOutputStream(keysFile, true)) { //open in append mode
-            try (FileChannel chan = fout.getChannel()) {
-                String line = key + "," + startPos + "," + finishPos + "\n"; // use text for ease of troubleshooting
-                ByteBuffer bb = ByteBuffer.wrap(line.getBytes());
-                chan.write(bb);
+    public DbItem put(String key, byte[] val) throws FileNotFoundException, IOException {
+        lock.writeLock().lock();
+        try {
+            if (mapOfItems.containsKey(key)) {
+                throw new RuntimeException("Key " + key + " is already present");
             }
+
+            long startPos;
+            long finishPos;
+            try (FileOutputStream fout = new FileOutputStream(valuesFile, true)) { //open in append mode
+                FileChannel chan = fout.getChannel();
+                startPos = chan.position();
+                ByteBuffer bb = ByteBuffer.wrap(val);
+                chan.write(bb);
+                finishPos = chan.position();
+            }
+            long length = finishPos - startPos;
+            if (length != val.length) {
+                throw new RuntimeException("Inserting blob into simplefiledb failed, lengths differ. Should be " + val.length + " but is " + length + ", for key=" + key);
+            }
+            DbItem dbItem = new DbItem(startPos, finishPos);
+
+            log.info("put: start={} finish={} key={}", startPos, finishPos, key);
+            try (FileOutputStream fout = new FileOutputStream(keysFile, true)) { //open in append mode
+                try (FileChannel chan = fout.getChannel()) {
+                    String line = key + "," + startPos + "," + finishPos + "\n"; // use text for ease of troubleshooting
+                    ByteBuffer bb = ByteBuffer.wrap(line.getBytes());
+                    chan.write(bb);
+                }
+            }
+
+            mapOfItems.put(key, dbItem);
+
+            return dbItem;
+        } finally {
+            lock.writeLock().unlock();
         }
-
-        mapOfItems.put(key, dbItem);
-
-        return dbItem;
-
     }
 
     public byte[] get(String key) throws FileNotFoundException, IOException {
-        DbItem item = mapOfItems.get(key);
-        if (item == null) {
-            return null;
+        lock.readLock().lock();
+        try {
+            DbItem item = mapOfItems.get(key);
+            if (item == null) {
+                return null;
+            }
+            return getLocked(item);
+        } finally {
+            lock.readLock().unlock();
         }
-        return get(item);
     }
 
     public byte[] get(DbItem item) throws FileNotFoundException, IOException {
+        lock.readLock().lock();
+        try {
+            return getLocked(item);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    // must only be called while holding either lock
+    private byte[] getLocked(DbItem item) throws FileNotFoundException, IOException {
         RandomAccessFile raf = new RandomAccessFile(valuesFile, "r");
         try (FileChannel chan = raf.getChannel()) {
             int size = (int) (item.finish - item.start);
